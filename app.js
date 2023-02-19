735aa88c21f3990a5a6cdc23c210f39ed2cb77c7
Tal Sabadia <69687310+W01v3n@users.noreply.github.com>
2023-02-19 08:44:13 +0200

    Update port on app.js to env var.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const listenPort = process.env.BLOG_V1_PORT;
 
 app.set('view engine', 'ejs');
 
@@ -63,6 +64,6 @@ app.post("/compose", function (req, res) {
 
 
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(listenPort, function () {
+    console.log("Server started on port " + listenPort + ".");
 });
